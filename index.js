21f90975e2dd501718e0ad7a3ae3108c78d35116
Ssy4a <sychevmrak@gmail.com>
2022-05-20 23:56:22 +0300

    v1

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require("express")
 const mongoose = require("mongoose")
 const authRouter = require("./authRouter")
 const testsRouter = require("./testsRouter")
-const PORT = process.env.PORT || 5000
-const cors = require("cors")
 
 const app = express()
 
